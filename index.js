83a1322180a50185fe6a213100641de98ed345ac
Kai Henningsen <kai.extern+git@gmail.com>
2020-01-18 01:14:48 +0100

    Initial checkin

---
+++ index.js
@@ -0,0 +1,172 @@
+"use strict";
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+function div(id, class_, contents) {
+    var ret = __spreadArrays(["div", {}], contents);
+    if (class_) {
+        ret[1].class = class_;
+    }
+    if (id) {
+        ret[1].id = id;
+    }
+    return ret;
+}
+exports.div = div;
+function span(id, class_, contents) {
+    var ret = __spreadArrays(["span", {}], contents);
+    if (class_) {
+        ret[1].class = class_;
+    }
+    if (id) {
+        ret[1].id = id;
+    }
+    return ret;
+}
+exports.span = span;
+var ents0 = require("html-entities");
+var ents = ents0.Html4Entities;
+var void_1 = __importDefault(require("html-tags/void"));
+var assert_1 = __importDefault(require("assert"));
+var singular = {
+    "<if>": 1,
+    "<!>": 1,
+    "<=>": 1,
+};
+for (var _i = 0, voidHtmlTags_1 = void_1.default; _i < voidHtmlTags_1.length; _i++) {
+    var i = voidHtmlTags_1[_i];
+    singular[i] = 1;
+}
+function classof(obj) {
+    return typeof obj === "object" ? (obj === null ? "null" : Object.getPrototypeOf(obj).constructor.name) : typeof obj;
+}
+function enc(s, who) {
+    var s1 = String(s);
+    var lines = s1.split("\n");
+    lines = lines.map(ents.encode);
+    return lines.join("\n");
+}
+// In-memory format of HTML:
+// tree = 'string'
+// tree = [
+//   'name',
+//   { attr: val, attr: val,},
+//   subtree,
+//   subtree,
+// ]
+function mkhtml(tree) {
+    if (typeof tree !== "object" || !tree) {
+        return [enc(tree, "tree is string")];
+    }
+    else if (classof(tree) === "Array" && (tree.length === 0 || classof(tree[0]) === "Array")) {
+        var arr = [];
+        for (var _i = 0, _a = tree; _i < _a.length; _i++) {
+            var e = _a[_i];
+            var res = mkhtml(e);
+            arr = arr.concat(res);
+        }
+        return arr;
+    }
+    else {
+        var name_1 = tree[0];
+        var attrs = tree[1];
+        var arr = [];
+        arr.push("<", name_1);
+        for (var attr in attrs) {
+            arr.push(" ", attr, '="', enc(attrs[attr], "attribute value"), '"');
+        }
+        arr.push(">");
+        switch (name_1) {
+            case "<if>":
+                arr = []; /* reset */
+                var condition = attrs.expr;
+                arr.push("<!--[if " + condition + "]>");
+                for (var _b = 0, _c = tree.slice(2); _b < _c.length; _b++) {
+                    var e2 = _c[_b];
+                    var res = mkhtml(e2);
+                    arr = arr.concat(res);
+                }
+                arr.push("<![endif]-->");
+                break;
+            case "<!>":
+                assert_1.default(tree.length === 3, "Bad length, usage ['<!>', {}, 'the comment']");
+                arr = ["<!--", enc(tree[2], "comment"), "-->"];
+                break;
+            case "<cdata>":
+                assert_1.default(tree.length === 3, "Bad length, usage ['<cdata>', {}, 'the data']");
+                var txt = tree[2];
+                assert_1.default(txt.indexOf("]]>") === -1, "CDATA cannot contain ]]>");
+                arr.push("<![CDATA[");
+                arr.push(txt);
+                arr.push("]]>");
+                break;
+            case "<=>":
+                assert_1.default(tree.length === 3, "Bad length, usage ['<=>', {}, 'the type']");
+                arr = ["<!DOCTYPE ", tree[2], ">"];
+                break;
+            default:
+                for (var _d = 0, _e = tree.slice(2); _d < _e.length; _d++) {
+                    var e2 = _e[_d];
+                    var res = mkhtml(e2);
+                    arr = arr.concat(res);
+                }
+                break;
+        }
+        if (!singular[name_1]) {
+            arr.push("</", name_1, ">");
+        }
+        return arr;
+    }
+}
+exports.mkhtml = mkhtml;
+// Mergeable tree format:
+// tree = 'string'
+// tree = ['$pname', _] => recurse tree params[pname]
+// tree = [
+//   'name',
+//   { attr: val, attr: val,},
+//   { attr: val, '$pname': _ } => { aname, avalue } = params[pname]
+//   subtree,
+//   subtree,
+// ]
+// Params format:
+// { pname: tree, pname: attrib, }
+function mergetree(tree, params) {
+    if (typeof tree !== "object" || !tree) {
+        return String(tree);
+    }
+    else {
+        var name_2 = tree[0];
+        var attrs = tree[1];
+        var arr = [name_2, {}];
+        if (name_2[0] === "$" && tree.length === 2) {
+            var p = params[name_2.slice(1)];
+            return mergetree(p, params);
+        }
+        for (var attr in attrs) {
+            var v = attrs[attr];
+            if (attr[0] === "$") {
+                var p = params[attr.slice(1)];
+                arr[1][p[0]] = p[1];
+            }
+            else {
+                arr[1][attr] = v;
+            }
+        }
+        for (var _i = 0, _a = tree.slice(2); _i < _a.length; _i++) {
+            var e2 = _a[_i];
+            var res = mergetree(e2, params);
+            arr.push(res);
+        }
+        return arr;
+    }
+}
+exports.mergetree = mergetree;
---
+++ index.ts
@@ -0,0 +1,167 @@
+"use strict";
+
+// $Id: htmlgen.ts 24 2020-01-12 15:09:51Z kai $
+
+type JHash = { [name: string]: string };
+type JElement = string | JTree;
+type JArrayElement = JHash | JElement;
+type JArray = JArrayElement[];
+interface JTree extends JArray {
+  0: string;
+  1: JHash;
+  // default: JElement;
+}
+
+export function div(id: string, class_: string, contents: JElement[]) {
+  let ret: JTree = ["div", {}, ...contents];
+  if (class_) {
+    ret[1].class = class_;
+  }
+  if (id) {
+    ret[1].id = id;
+  }
+  return ret;
+}
+export function span(id: string, class_: string, contents: JElement[]) {
+  let ret: JTree = ["span", {}, ...contents];
+  if (class_) {
+    ret[1].class = class_;
+  }
+  if (id) {
+    ret[1].id = id;
+  }
+  return ret;
+}
+
+import ents0 = require("html-entities");
+const ents = ents0.Html4Entities;
+import voidHtmlTags from "html-tags/void";
+import assert from "assert";
+
+const singular: { [s: string]: 1 } = {
+  "<if>": 1,
+  "<!>": 1,
+  "<=>": 1,
+};
+for (let i of voidHtmlTags) {
+  singular[i] = 1;
+}
+
+function classof(obj: any) {
+  return typeof obj === "object" ? (obj === null ? "null" : Object.getPrototypeOf(obj).constructor.name) : typeof obj;
+}
+
+function enc(s: any, who: string) {
+  let s1 = String(s);
+  let lines = s1.split("\n");
+  lines = lines.map((ents as any).encode);
+  return lines.join("\n");
+}
+
+// In-memory format of HTML:
+// tree = 'string'
+// tree = [
+//   'name',
+//   { attr: val, attr: val,},
+//   subtree,
+//   subtree,
+// ]
+export function mkhtml(tree: JElement): string[] {
+  if (typeof tree !== "object" || !tree) {
+    return [enc(tree, "tree is string")];
+  } else if (classof(tree) === "Array" && (tree.length === 0 || classof(tree[0]) === "Array")) {
+    let arr: string[] = [];
+    for (const e of tree as JElement[]) {
+      const res = mkhtml(e);
+      arr = arr.concat(res);
+    }
+    return arr;
+  } else {
+    const name = tree[0];
+    const attrs = tree[1];
+    let arr: string[] = [];
+    arr.push("<", name);
+    for (const attr in attrs) {
+      arr.push(" ", attr, '="', enc(attrs[attr], "attribute value"), '"');
+    }
+    arr.push(">");
+    switch (name) {
+      case "<if>":
+        arr = []; /* reset */
+        let condition = attrs.expr;
+        arr.push("<!--[if " + condition + "]>");
+        for (const e2 of tree.slice(2) as JElement[]) {
+          const res = mkhtml(e2);
+          arr = arr.concat(res);
+        }
+        arr.push("<![endif]-->");
+        break;
+      case "<!>":
+        assert(tree.length === 3, "Bad length, usage ['<!>', {}, 'the comment']");
+        arr = ["<!--", enc(tree[2], "comment"), "-->"];
+        break;
+      case "<cdata>":
+        assert(tree.length === 3, "Bad length, usage ['<cdata>', {}, 'the data']");
+        const txt = tree[2] as string;
+        assert(txt.indexOf("]]>") === -1, "CDATA cannot contain ]]>");
+        arr.push("<![CDATA[");
+        arr.push(txt);
+        arr.push("]]>");
+        break;
+      case "<=>":
+        assert(tree.length === 3, "Bad length, usage ['<=>', {}, 'the type']");
+        arr = ["<!DOCTYPE ", tree[2] as string, ">"];
+        break;
+      default:
+        for (const e2 of tree.slice(2) as JElement[]) {
+          const res = mkhtml(e2);
+          arr = arr.concat(res);
+        }
+        break;
+    }
+    if (!singular[name]) {
+      arr.push("</", name, ">");
+    }
+    return arr;
+  }
+}
+
+// Mergeable tree format:
+// tree = 'string'
+// tree = ['$pname', _] => recurse tree params[pname]
+// tree = [
+//   'name',
+//   { attr: val, attr: val,},
+//   { attr: val, '$pname': _ } => { aname, avalue } = params[pname]
+//   subtree,
+//   subtree,
+// ]
+// Params format:
+// { pname: tree, pname: attrib, }
+export function mergetree(tree: string | number | JTree, params: { [x: string]: any }): JElement {
+  if (typeof tree !== "object" || !tree) {
+    return String(tree);
+  } else {
+    const name = tree[0];
+    const attrs = tree[1];
+    const arr: JTree = [name, {}];
+    if (name[0] === "$" && tree.length === 2) {
+      const p = params[name.slice(1)];
+      return mergetree(p, params);
+    }
+    for (const attr in attrs) {
+      const v = attrs[attr];
+      if (attr[0] === "$") {
+        const p = params[attr.slice(1)];
+        arr[1][p[0]] = p[1];
+      } else {
+        arr[1][attr] = v;
+      }
+    }
+    for (const e2 of tree.slice(2) as JElement[]) {
+      const res = mergetree(e2, params);
+      arr.push(res);
+    }
+    return arr;
+  }
+}
